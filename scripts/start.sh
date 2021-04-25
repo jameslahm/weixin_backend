f632b765a88346ae223fe317fb0c04ee4605cb67
wangao <1366463855@qq.com>
2021-04-25 15:10:06 +0800

    chore: add docker image start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 git pull
 mvn install
-pid=$(ps -ef | grep -v "grep" java | awk '{print $2}')
-kill -9 $(pid)
+pid=$(ps -ef | grep java | grep -v 'grep' | awk '{print $2}')
+if test -z "$pid" then
+    kill -9 $(pid)
+fi
 nohup java -jar course-0.0.1.jar &
 
