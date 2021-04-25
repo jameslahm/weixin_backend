58696535c0502f7e0c034717db20525dfea05f76
wangao <1366463855@qq.com>
2021-04-25 15:13:17 +0800

    chore: add docker image start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+echo "start git pull..."
 git pull
+echo "git pull done!"
+
+echo "start mvn install"
 mvn install
+echo "mvn install donw"
+
+echo "start kill previouse pid"
 pid=$(ps -ef | grep java | grep -v 'grep' | awk '{print $2}')
 if test -z "$pid" then
+    echo "kill $(pid)"
     kill -9 $(pid)
 fi
+echo "kill previous pid down"
+echo "start new pid"
 nohup java -jar course-0.0.1.jar &
-
+echo "start new pid done"
