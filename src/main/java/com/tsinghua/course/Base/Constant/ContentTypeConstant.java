1341eb5ff7a98e93ab94242742c145c45138b296
jameslahm <1366463855@qq.com>
2021-05-02 20:53:18 +0800

    feat: init

--- src/main/java/com/tsinghua/course/Base/Constant/ContentTypeConstant.java
+++ src/main/java/com/tsinghua/course/Base/Constant/ContentTypeConstant.java
@@ -4,12 +4,14 @@ public class ContentTypeConstant {
     public final static String TEXT = "TEXT";
     public final static String AUDIO = "AUDIO";
     public final static String VIDEO = "VIDEO";
+    public final static String IMAGE = "IMAGE";
 
     static public boolean checkContentTypeValid(String contentType){
         switch (contentType){
             case TEXT:
             case AUDIO:
             case VIDEO:
+            case IMAGE:
                 return true;
             default:
                 return false;
